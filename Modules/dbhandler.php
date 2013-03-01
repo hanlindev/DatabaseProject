b1adb3996c772adbff09e317779da2437ddaca49
Han Lin <hanlin.dev@gmail.com>
2013-03-01 14:25:47 +0800

    Complete test class methods and index page

--- Modules/dbhandler.php
+++ Modules/dbhandler.php
@@ -13,15 +13,21 @@ class dbhandler {
 	 * sendQuery
 	 * @param $queryContent is the complete content of the query as
 	 *        a string
+	 * @param result of query, false on failure, mysqli_result object
+	 *        on successful SELECT, SHOW, DESCRIBE, EXPLAIN queries,
+	 *        and true on other successful queries.
 	 * Send the query to our server. This is the backend method. So
 	 * you should not use this to send your query directly. Use the
 	 * public methods instead.
 	 */
 	private static function sendQuery($queryContent) {
-		mysql_connect($server,$username, $password);
-		@mysql_select_db($database) or die("Unable to select database");
-		mysql_query($queryContent);
-		mysql_close();
+		$mysqli = new mysqli($server, $username, $password, $database);
+		if ($mysqli->connect_error) {
+			die('Connect error ('.$mysqli->connect_errno.') '.$mysqli->connect_error);
+		}
+		$queryResult = $mysqli->query($queryContent);
+		$mysqli->close();
+		return $queryResult;
 	}
 
 	/**
@@ -31,14 +37,46 @@ class dbhandler {
 	 *        is going to be inserted. Please refer to the specification
 	 *        of hotel table for the order of attributes. You have to
 	 *        strictly follow the order of attributes!
+	 * @return returns the result of sendQuery method
 	 * A generic method used for inserting attributes to a specified
 	 * table.
 	 */
 	private static function insertIntoTable($tableName, $rowInfo) {
-		
+		$queryContent = "INSERT INTO $tableName VALUES(";
+		$comma = "";
+		foreach ($rowInfo as $info) {
+			$queryContent .= $comma.$info;
+			$comma = ",";
+		}
+		$queryContent .= ");";
+		return sendQuery($queryContent);
+	}
+
+	/**
+	 * generateRowInfoArray
+	 * @param a variable-length list of attributes in the order specified by
+	 *        the table definition.
+	 * @return the info array containing the attributes
+	 * We can do this in all our public methods but it's not a good idea.
+	 * We'd better have a strict rule on parameter passing for those methods.
+	 */
+	private static function generateRowInfoArray() {
+		$attributes = func_get_args();
+		return $attributes;
+	}
+
+	/**
+	 * insertIntoHotel
+	 * @param attributes of a hotel
+	 * @return return result of sendQuery method
+	 * public interface to insert a row into hotel table
+	 */
+	public static function insertIntoHotel($name, $location, $star, $swimming_pool, $fitness_club, $buffet_restaurant, $restaurant) {
+		$rowInfo = generateRowInfoArray($name, $location, $star, $swimming_pool, $fitness_club, $buffet_restaurant, $restaurant);
+		return insertIntoTable("hotel", $rowInfo);
 	}
 
 	public static function testClass($username, $password) {
-		echo "<p>lalala ".$username." hohoho ".$password." wuwuwuwu</p>";
+		echo "<p>lalala( ".$username." hohoho ".$password." wuwuwuwu</p>";
 	}
 }
